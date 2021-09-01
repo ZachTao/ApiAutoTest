202c2c6e629d63078dc31ea350382665f5d8974a
ZachTao <429529484@qq.com>
2021-09-01 11:52:42 +0800

    0901 backup

---
+++ splitlist.py
@@ -0,0 +1,18 @@
+# -*-coding:utf-8 -*-
+def list_of_groups(list_info, per_list_len):
+    '''
+    :param list_info:   列表
+    :param per_list_len:  每个小列表的长度
+    :return:
+    '''
+    list_of_group = zip(*(iter(list_info),) * per_list_len)
+    end_list = [list(i) for i in list_of_group]  # i is a tuple
+    count = len(list_info) % per_list_len
+    end_list.append(list_info[-count:]) if count != 0 else end_list
+    return end_list
+
+
+if __name__ == '__main__':
+    list_info = ['name Rookey', 'age 10', 'sex man', 'name lisi', 'age 11', 'sex women']
+    ret = list_of_groups(list_info, 3)
+    print(ret)
